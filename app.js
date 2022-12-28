7c8431cd94e01b18070df68e0fafb7174423894d
malikali3 <malikalistudent@gmail.com>
2022-12-28 20:25:05 +0500

    added first files

---
+++ app.js
@@ -0,0 +1,50 @@
+const navbar = document.querySelector('.navbar')
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const navLinksLi = document.querySelectorAll('.nav-links li')
+
+
+window.addEventListener('scroll', () => {
+    if(this.scrollY >= 100) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+})
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
+
+navLinksLi.forEach(li => li.addEventListener('click', ()=> {
+    navLinksLi.forEach(li => li.classList.remove('active'))
+    li.classList.add('active')
+
+    hamburger.classList.remove('active')
+    navLinks.classList.remove('active')
+
+}))
+
+
+// Typed js
+var options = {
+    strings: [
+        'UX Designer', 
+        'Programmer',
+        'Educator',
+        'Your Growth Partner'
+    ],
+    typeSpeed: 120,
+    loop: true,
+    backSpeed:50,
+    loopCount: Infinity,
+    backDelay: 3000,
+    
+    
+  };
+  
+var typed = new Typed('#hero-titles', options);
+
+// AOS
+AOS.init();
